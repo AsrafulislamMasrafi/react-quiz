1cff1954119aaf86b0237d92d51671be1c528008
Asraful Islam Masrafi <asrafulislammasrafi@gmail.com>
2024-02-03 17:02:51 +0600

    progressBar fix

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,29 +1,45 @@
+import { useRef, useState } from "react";
+
 export function ProgressBar({ nextBtn, width, previousBtn, nextQ }) {
+  const [toolTip, setToolTip] = useState(false);
+  const toolTipRef = useRef();
+
+  function toggleToolTip() {
+    if (toolTip) {
+      setToolTip(false);
+      toolTipRef.current.style.display = "none";
+    } else {
+      setToolTip(true);
+      toolTipRef.current.style.left = ` calc(${width} - 65px)`;
+      toolTipRef.current.style.display = "block";
+    }
+  }
   return (
     <div className="progressBar">
       <div className="backButton">
         <span className="material-icons-outlined" onClick={previousBtn}>
-          {" "}
-          arrow_back{" "}
+          arrow_back
         </span>
       </div>
       <div className="rangeArea">
-        <div className="tooltip">24% Complete!</div>
+        <div className="tooltip" ref={toolTipRef}>
+          {width} Complete!
+        </div>
         <div className="rangeBody">
           <div
             className="progress"
             style={{
               width: `${width}`,
             }}
+            onMouseOver={toggleToolTip}
+            onMouseOut={toggleToolTip}
           ></div>
         </div>
       </div>
-      {/* <Link to="/result"> */}
       <button className="button next" onClick={nextBtn}>
         <span>{nextQ}</span>
         <span className="material-icons-outlined"> arrow_forward </span>
       </button>
-      {/* </Link> */}
     </div>
   );
 }
